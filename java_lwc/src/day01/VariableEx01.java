b27c393e324720729df6e8be112967443fc7271f
limwoochul <wclim0319@naver.com>
2024-05-28 17:13:31 +0900

    Update VariableEx01.java

--- java_lwc/src/day01/VariableEx01.java
+++ java_lwc/src/day01/VariableEx01.java
@@ -20,8 +20,12 @@ public class VariableEx01 {
 		System.out.println(ch6);
 		System.out.println(ch5);
 		
-		long num1 = 1234567890123l;//정수값이 int형으로 표현되지 않으면 접미사를 반드시 붙여야 합니다.
-		System.out.println(num1);
+		//정수 리터럴은 기본이 int이고 실수 리터럴은 기본이 double이다.
+		
+		long num1 = 1234567890123L;//정수값이 int형으로 표현되지 않으면 접미사를 반드시 붙여야 합니다.
+		float num2 = 3.14F;//3.14는 double형이기 때문에 반드시 접미사를 붙여야 합니다.
+		double num3 = 3.14;
+		boolean isEven = true;//소문자로만 입력
 	}
 
 }
