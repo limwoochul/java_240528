10c140569ef4cb6f1f4bbbd9b68bfd02710775c6
limwoochul <wclim0319@naver.com>
2024-07-24 16:47:37 +0900

    db 41일차 DCL 추가

--- db/04일차_DCL.sql
+++ db/04일차_DCL.sql
@@ -16,4 +16,21 @@ SET PASSWORD FOR 'cgv_admin'@'%' = 'admin1234';
 # DROP USER '아이디'@'호스트명'
 DROP USER 'cgv_admin'@'%';
 
+# 계정조회
 SELECT USER, HOST FROM MYSQL.USER;
+
+# 권한 부여
+# 사용자에게 DB에 대한 권한을 부여, 데이터 추가, 수정, 삭제, 테이블 추가, 수정, 삭제 등
+# SELECT/INSERT/UPDATE/DELETE/CREATE/ALTER/DROP/REFERENCES/ALL PRIVILEGES(모든 권한)
+# GRANT 권한종류 ON DB명.테이블명 TO '아이디'@'호스트명';
+# cgv_admin 계정에 cgv DB는 모든 권한을 부여하고, 다른 DB에는 조회 권한을 부여
+GRANT ALL PRIVILEGES ON CGV.* TO 'cgv_admin'@'%';
+GRANT SELECT ON PRODUCT.* TO 'cgv_admin'@'%';
+GRANT SELECT ON STUDENT.* TO 'cgv_admin'@'%';
+GRANT SELECT ON UNIVERSITY.* TO 'cgv_admin'@'%';
+
+# 권한 제거
+# 사용자에게 부여했던 DB에 대한 권한을 제거
+# REVOKE 권한 ON DB명.테이블명 FROM '아이디'@'호스트명';
+# PRODUCT DB에 SELECT 권한을 제거
+REVOKE SELECT ON PRODUCT.* FROM 'cgv_admin'@'%';
