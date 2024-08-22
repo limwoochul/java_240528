7e22f058303b33486426149fa1bf02a8a89e73c5
limwoochul <wclim0319@naver.com>
2024-08-22 22:52:59 +0900

    Create 2차프로젝트_학사관리시스템_DDL.sql

---
+++ db/2차프로젝트_학사관리시스템_DDL.sql
@@ -0,0 +1,139 @@
+DROP DATABASE IF EXISTS UNIVERSITY_MANAGEMENT;
+
+CREATE DATABASE IF NOT EXISTS UNIVERSITY_MANAGEMENT;
+
+USE UNIVERSITY_MANAGEMENT;
+
+DROP TABLE IF EXISTS `member`;
+
+CREATE TABLE `member` (
+	`me_id`	varchar(10) primary key	NOT NULL,
+	`me_pw`	varchar(255)			NOT NULL DEFAULT 1234,
+	`me_name`	varchar(30)			NOT NULL,
+	`me_email`	varchar(50) unique	NOT NULL,
+	`me_authority`	varchar(10)		NOT NULL
+);
+
+DROP TABLE IF EXISTS `major`;
+
+CREATE TABLE `major` (
+	`ma_num`	int primary key auto_increment	NOT NULL,
+	`ma_name`	varchar(30)						NOT NULL
+);
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+	`st_num`	int primary key auto_increment	NOT NULL,
+	`st_me_id`	varchar(10)						NOT NULL,
+	`st_ma_num`	int								NOT NULL
+);
+
+DROP TABLE IF EXISTS `professor`;
+
+CREATE TABLE `professor` (
+	`pr_num`	int primary key auto_increment	NOT NULL,
+	`st_me_id`	varchar(10)						NOT NULL,
+	`st_ma_num`	int								NOT NULL
+);
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+	`ad_num`	int primary key auto_increment	NOT NULL,
+	`me_id`	varchar(10)							NOT NULL
+);
+
+DROP TABLE IF EXISTS `lecture`;
+
+CREATE TABLE `lecture` (
+	`le_num`	int primary key auto_increment	NOT NULL,
+	`le_name`	varchar(30)						NOT NULL,
+	`le_room`	varchar(50)						NOT NULL,
+	`le_time`	varchar(30)						NOT NULL,
+	`le_point`	int								NOT NULL,
+	`pr_num`	int								NOT NULL
+);
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+	`co_num`	int primary key auto_increment	NOT NULL,
+	`co_grade`	varchar(5)						NOT NULL DEFAULT 1,
+	`st_num`	int								NOT NULL,
+	`le_num`	int								NOT NULL
+);
+
+DROP TABLE IF EXISTS `notice`;
+
+CREATE TABLE `notice` (
+	`no_num`	int primary key auto_increment	NOT NULL,
+	`no_title`	varchar(100)					NOT NULL,
+	`no_content`	longtext					NOT NULL,
+	`no_date`	datetime						NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`no_view`	int								NOT NULL DEFAULT 0,
+	`ad_num`	int								NOT NULL
+);
+
+ALTER TABLE `student` ADD CONSTRAINT `FK_member_TO_student_1` FOREIGN KEY (
+	`st_me_id`
+)
+REFERENCES `member` (
+	`me_id`
+);
+
+ALTER TABLE `student` ADD CONSTRAINT `FK_major_TO_student_1` FOREIGN KEY (
+	`st_ma_num`
+)
+REFERENCES `major` (
+	`ma_num`
+);
+
+ALTER TABLE `professor` ADD CONSTRAINT `FK_member_TO_professor_1` FOREIGN KEY (
+	`st_me_id`
+)
+REFERENCES `member` (
+	`me_id`
+);
+
+ALTER TABLE `professor` ADD CONSTRAINT `FK_major_TO_professor_1` FOREIGN KEY (
+	`st_ma_num`
+)
+REFERENCES `major` (
+	`ma_num`
+);
+
+ALTER TABLE `admin` ADD CONSTRAINT `FK_member_TO_admin_1` FOREIGN KEY (
+	`me_id`
+)
+REFERENCES `member` (
+	`me_id`
+);
+
+ALTER TABLE `lecture` ADD CONSTRAINT `FK_professor_TO_lecture_1` FOREIGN KEY (
+	`pr_num`
+)
+REFERENCES `professor` (
+	`pr_num`
+);
+
+ALTER TABLE `course` ADD CONSTRAINT `FK_student_TO_course_1` FOREIGN KEY (
+	`st_num`
+)
+REFERENCES `student` (
+	`st_num`
+);
+
+ALTER TABLE `course` ADD CONSTRAINT `FK_lecture_TO_course_1` FOREIGN KEY (
+	`le_num`
+)
+REFERENCES `lecture` (
+	`le_num`
+);
+
+ALTER TABLE `notice` ADD CONSTRAINT `FK_admin_TO_notice_1` FOREIGN KEY (
+	`ad_num`
+)
+REFERENCES `admin` (
+	`ad_num`
+);
