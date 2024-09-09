9aa953be0b26b080ef116f1d77fe05620804a897
limwoochul <wclim0319@naver.com>
2024-09-09 21:46:32 +0900

    D71 리액트 1일차 : 리액트 프로젝트 생성 및 Button 컴포넌트 생성 예제

---
+++ react/react1/src/App.js
@@ -0,0 +1,15 @@
+//import './css/App.css'; //css폴더에 App.css가 있고, 연결할 때 사용
+
+import Button from "./Button";
+
+function App() {
+
+  return (
+    <div>
+      <button>버튼</button>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
