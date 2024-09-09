a0c07c2815b10b38365a6f116e16117dc12940f7
limwoochul <wclim0319@naver.com>
2024-09-09 23:58:20 +0900

    D71 리액트 1일차 : 예제 - 오늘의 할일(입력창으로 할일 1개 입력)

--- react/test1/src/App.js
+++ react/test1/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Test1 from './test1/Test1';
+import Todo from './todo/Todo';
+import TodoList from './todoList/TodoList';
 
 function App() {
   return (
     <div>
       {/* <Test1 /> */}
-
+      {/* <Todo /> */}
+      <TodoList />
     </div>
   );
 }
