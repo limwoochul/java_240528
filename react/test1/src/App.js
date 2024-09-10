00819e166978aa95b343a5b9b3adcc4df81bada4
limwoochul <wclim0319@naver.com>
2024-09-10 16:51:22 +0900

    D72 리액트 2일차 : 예제 - 과일선택

--- react/test1/src/App.js
+++ react/test1/src/App.js
@@ -4,6 +4,7 @@ import ButtonBox2 from './buttonEx/ButtonBox2';
 import ButtonBox3 from './buttonEx/ButtonBox3';
 import Input1 from './input/Input1';
 import Input2 from './input/Input2';
+import Select from './input/Select';
 import List1 from './list/List1';
 import Test1 from './test1/Test1';
 import Todo from './todo/Todo';
@@ -20,7 +21,8 @@ function App() {
       {/* <ButtonBox3 /> */}
       {/* <Input1 /> */}
       {/* <Input2 /> */}
-      <List1 />
+      {/* <List1 /> */}
+      <Select />
     </div>
   );
 }
