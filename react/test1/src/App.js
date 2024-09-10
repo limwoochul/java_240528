de5113b9509135fea82e61128b883467c97712dd
limwoochul <wclim0319@naver.com>
2024-09-10 10:18:47 +0900

    D72 리액트 2일차 : 예제 - 버튼 페이지네이션

--- react/test1/src/App.js
+++ react/test1/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ButtonBox from './buttonEx/ButtonBox';
 import Test1 from './test1/Test1';
 import Todo from './todo/Todo';
 import TodoList from './todoList/TodoList';
@@ -8,7 +9,8 @@ function App() {
     <div>
       {/* <Test1 /> */}
       {/* <Todo /> */}
-      <TodoList />
+      {/* <TodoList /> */}
+      <ButtonBox />
     </div>
   );
 }
