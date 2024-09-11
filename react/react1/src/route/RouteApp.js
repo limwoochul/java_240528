9b22f3febed2d34a78c4133a395e791ee2428ae2
limwoochul <wclim0319@naver.com>
2024-09-11 15:55:23 +0900

    D73 리액트 3일차 : 예제 - 라우터를 이용한 게시글 예제

--- react/react1/src/route/RouteApp.js
+++ react/react1/src/route/RouteApp.js
@@ -1,13 +1,14 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Link, Routes, Route} from 'react-router-dom';
 import Home from './Home';
 import Nav from './Nav';
-import PostList from './PostList';
 import PostInsert from './PostInsert';
+import PostList from './PostList';
 import { useState } from 'react';
+import PostDetail from './PostDetail';
 
 function RouteApp(){
 	let links = ['/', '/post/insert', '/post/list'];
-	let titles = ['홈', '게시글 등록', '게시글 목록'];
+	let titles = ['홈', '게시글 등록', '게시글 목록']
 	let [list, setList] = useState([
 		{
 			no : 2,
@@ -16,34 +17,43 @@ function RouteApp(){
 			writer : 'admin',
 			view : 0
 		},{
-			no : 1,
 			title : '공지',
 			content : '공지사항',
 			writer : 'admin',
 			view : 0
-		},
+		}
 	]);
-	return(
-		<div>
-			<BrowserRouter>
-				{/* 메뉴 */}
-				<Nav links={links} titles={titles}/>
-				{/* 메뉴에 맞는 컴포넌트를 연결 */}
-				<Body list={list} />
-			</BrowserRouter>
-		</div>
+	function addPost(post){
+		setList([post, ...list]);
+	}
+	function delPost(index){
+		setList(list.filter((item,i)=>index != i));
+	}
+	return (
+		<BrowserRouter>
+			{/* 메뉴 */}
+			<Nav links={links} titles={titles} />
+			{/* 메뉴에 맞는 컴포넌트를 연결 */}
+			<Body list={list} addPost={addPost} delPost={delPost}/>
+		</BrowserRouter>
 	)
 }
-function Body({list}){
-	return(
+function Body({list, addPost, delPost}){
+	return (
 		<Routes>
 			<Route path={"/"} exact element={<Home/>}/>
 			<Route 
 				path={"/post/list"} 
 				element={
-					<PostList list={list}/>}
+					<PostList 
+						list={list} 
+						addPost={addPost}
+						delPost={delPost}
+					/>
+				}
 			/>
 			<Route path={"/post/insert"} element={<PostInsert/>}/>
+			<Route path={"/post/detail/:id"} element={<PostDetail list={list}/>}/>
 		</Routes>
 	)
 }
